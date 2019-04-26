d85d0432f44d1a58d7f91e163cdd8cb867976736
2j2-1 <jbrightmore@protonmail.com>
2019-04-26 14:32:45 +0100

    Added stringBuffer

--- TSPM/TSPM/TSPM.cpp
+++ TSPM/TSPM/TSPM.cpp
@@ -16,7 +16,7 @@ int search() {
 	return 5;
 }
 
-int menu(std::string str) {
+int menu() {
 	int offset = 1;
 	int choice = -1;
 	game.print("1. Load File",3,3 + offset);
@@ -26,16 +26,17 @@ int menu(std::string str) {
 	game.print("5. Search", 3, 7 + offset);
 	game.print("6. Maintenance", 3, 8 + offset);
 	game.print("0. Exit", 3, 9 + offset);
-	game.print("Please Enter Choice: " + str, 3, 11 + offset);
+	game.print("Please Enter Choice: " + game.stringBuffer, 3, 11 + offset);
 	if (GetAsyncKeyState(VK_RETURN)) {
-		return std::stoi(str);
+		int temp = std::stoi(game.stringBuffer);
+		game.stringBuffer.clear();
+		return temp;
 	}
 	return -1;
 }
 
 int main(){
 	int screen = -1;
-	std::string str;
 	game.setup();
 	game.blank_screen();
 	while (true) {
@@ -43,37 +44,37 @@ int main(){
 		game.border(2);
 		game.print("Welcome to TSPM",game.screenWidth/2-8,2);
 		game.draw_pixel(0, 0, char(screen+48));
-		str += game.input();
-		if (GetAsyncKeyState(VK_BACK) && str.size()>0) {
-			str.pop_back();
+		game.stringBuffer += game.input();
+		if (GetAsyncKeyState(VK_BACK) && game.stringBuffer.size()>0) {
+			game.stringBuffer.pop_back();
 		}
 		switch (screen) {
 		case -1:
-			screen = menu(str);
+			screen = menu();
 			break;
 		case 0:
 			return 0;
 		case 1:
-
+			screen = -1;
 			break;
 		case 2:
-
+			screen = -1;
 			break;
 		case 3:
-
+			screen = -1;
 			break;
 		case 4:
-
+			screen = -1;
 			break;
 		case 5:
 			screen = search();
 			break;
 		case 6:
-
+			screen = -1;
 			break;
 		default:
-			screen = menu(str);
-			game.print("Invalid Choice Please Try Agan",3,13);
+			screen = menu();
+			game.print("Invalid Choice Please Try Again",3,13);
 		}
 		game.draw();
 		Sleep(150);
